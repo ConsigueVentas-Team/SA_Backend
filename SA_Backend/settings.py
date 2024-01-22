d9978aa753ef7ce8f2b92149ebfa53f5d42a33db
Duberly2004 <ivanmondragonmanchay@gmail.com>
2024-01-22 12:56:57 -0500

    Add: Create model User

--- SA_Backend/settings.py
+++ SA_Backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.exceptions',
     'rest_framework_simplejwt',
     'django_filters',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'api.User' # Indicamos el modelo de authentication para toda la api
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- api/model/UserModel.py
+++ api/model/UserModel.py
@@ -1,1 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin 
+
+class User(AbstractBaseUser,PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=40,unique=True)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    dni = models.CharField(max_length=9)
+    cellphone = models.CharField(max_length=30)
+    birthday = models.DateField()
+    # url_image = models.CharField(max_length=4000)
+    # position_id = models.ForeignKey
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)#Fecha de creacion
+    updated_at = models.DateTimeField(auto_now=True)#Fecha de actualización
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'surname','email']
+
+    def __str__(self):
+        return self.name
