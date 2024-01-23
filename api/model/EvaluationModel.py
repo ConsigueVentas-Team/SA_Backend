498773846b8c87891b0ff6236d771cb3b105e039
LuisBarrial <angeloxz.com@gmail.com>
2024-01-22 21:58:02 -0500

    actualizacion de models with user

--- api/model/EvaluationModel.py
+++ api/model/EvaluationModel.py
@@ -2,13 +2,13 @@ from api.model.UserModel import User
 from django.db import models
 
 class Evaluation(models.Model):
+    id = models.AutoField(primary_key=True)
     date = models.DateField()
-    user_id = models.ForeignKey(User, on_delete= models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     softskills = models.IntegerField()
     performance =models.IntegerField() 
     hardskills = models.IntegerField()
-    autoevaluacion =  models.DecimalField(max_digits=4, decimal_places=2)
+    autoevaluation =  models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    promedio = models.DecimalField(max_digits=4, decimal_places=2)
 
--- api/model/NotificationModel.py
+++ api/model/NotificationModel.py
@@ -2,7 +2,8 @@ from api.model.UserModel import User
 from django.db import models
 
 class Notification(models.Model):
-    message = models.TextField()
-    user_Id = models.ForeignKey(User, on_delete= models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    message = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
