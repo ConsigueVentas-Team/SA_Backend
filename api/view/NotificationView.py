a8af3fc8c4f53bab4aee99aeb72ebdb4edaa07b4
Duberly2004 <ivanmondragonmanchay@gmail.com>
2024-02-01 10:12:21 -0500

    Add: add pagination notification

--- api/view/NotificationView.py
+++ api/view/NotificationView.py
@@ -1,8 +1,9 @@
 from api.model.NotificationModel import Notification
 from api.serializers.NotificationSerializer import NotificationSerializer
 from rest_framework import generics, permissions
-
+from api.CustomPagination import *
 class NotificationListCreateView(generics.ListCreateAPIView):
   queryset = Notification.objects.all()
   serializer_class = NotificationSerializer
   permission_classes = [permissions.IsAuthenticated]
+  pagination_class = CustomPageNumberPagination
