dfbd2eb65f1c9796d42748afa5c6e1d04d9cc0b4
IsaacCRS17 <crs170919@gmail.com>
2024-02-07 11:37:53 -0500

    Add: comments

--- api/serializers/ScheduleSerializer.py
+++ api/serializers/ScheduleSerializer.py
@@ -6,8 +6,8 @@ class ScheduleSerializer(serializers.ModelSerializer):
         model = Schedule
         fields = ['dayOfWeek', 'startTime', 'endTime', 'user']
 
+    # Mapear los nombres de campos esperados en la solicitud a los nombres del modelo
     def to_internal_value(self, data):
-        # Mapear los nombres de campos esperados en la solicitud a los nombres del modelo
         internal_data = {
             'dayOfWeek': data['day'],
             'startTime': data['inicio'],
