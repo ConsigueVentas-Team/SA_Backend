a013087386673de49ebc843e7162988dc1095554
IsaacCRS17 <crs170919@gmail.com>
2024-02-16 10:07:13 -0500

    Fix: tildes

--- api/functions/getRol.py
+++ api/functions/getRol.py
@@ -2,7 +2,7 @@ def getRol(rol):
         if rol == 1:
             return {'id': 1, 'name': 'Gerencia'}
         elif rol == 2:
-            return {'id': 2, 'name': 'Líder Nucleo'}
+            return {'id': 2, 'name': 'Lider Nucleo'}
         elif rol == 3:
             return {'id': 3, 'name': 'Colaborador'}
         else:
